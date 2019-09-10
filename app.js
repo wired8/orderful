a0e0135f4a697e3fb20844f8e7383041f575ae37
PhilJ <phil@wired8.com>
2019-09-10 12:31:47 -0700

    Added diff-json to compare changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const axios = require('axios');
-//const { assert } = require('chai');
+const orders = require('./lib/orders');
+const jsonDiff = require('diff-json');
 
 const getData = async () => {
   try {
@@ -11,55 +12,11 @@ const getData = async () => {
   }
 };
 
-const processChanges = function(data) {
-  for (let i=0; i<data.orderChanges.length; ++i) {
-    let orderChange = data.orderChanges[i];
-    applyChanges(data.orders, orderChange);
-  }
-  return data.orders;
-};
-
-const applyChanges = function(orders, orderChange) {
-  for (let i=0; i<orderChange.changes.length; ++i) {
-    let change = orderChange.changes[i];
-    applyChange(orders,
-      orderChange.ownerId,
-      change.orderId,
-      change.itemId,
-      change.changeType,
-      change.changeProperty,
-      change.changeValue);
-  }
-};
-
-const applyChange = function(orders, ownerId, orderId, itemId, type, property, value) {
-  let o = orders.filter(x => {
-    return x.ownerId === ownerId && x.id === orderId;
-  })[0];
-
-  o.items = o.items.map(x => {
-    if (x.id === itemId) {
-      switch (type) {
-        case 'increase':
-          x[property] += value;
-          break;
-        case 'replace':
-          x[property] = value;
-          break;
-      }
-    }
-    return x;
-  });
-};
-
-
 const main = async () => {
   const result = await getData();
-  //console.log(JSON.stringify(result.data,null,2));
-  console.log( assert(Array.isArray([]), 'empty arrays are arrays') );
-  processChanges(result.data);
- // console.log(JSON.stringify(result.data,null,2));
-
+  const data = JSON.parse(JSON.stringify(result.data));
+  const after = orders.processOrders(data.orders, data.orderChanges);
+  console.log('Diff',JSON.stringify(jsonDiff.diff(result.data.orders,after), null, 4));
 };
 
 main();
--- test/orders.js
+++ test/orders.js
@@ -5,22 +5,28 @@ const chai = require('chai');
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
 const orders = require('../lib/orders');
+const jsonDiff = require('diff-json');
 
 chai.use(chaiHttp);
 
 describe('Download and parse orders', () => {
-  let token;
-
   it('should parse orders', (done) => {
     chai.request(host)
       .get('/tech-interview-example.json')
       .end((err, res) => {
         expect(err).be.null;
-        expect(res.body).to.be.a('object');
-        //console.log(JSON.stringify(res.body,null,2));
-        let data = res.body;
-        console.log(JSON.stringify(orders.processChanges(data),null,2));
         expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.a('object', 'blah');
+        const before = JSON.parse(JSON.stringify(res.body));
+        expect(before.orders[0].items[1].quantity).to.equal(10);
+        expect(before.orders[0].items[2].quantity).to.equal(200);
+
+        const after = orders.processOrders(before.orders, before.orderChanges);
+        expect(before.orders.length).to.equal(after.length);
+        expect(after[0].items[1].quantity).to.equal(20);
+        expect(after[0].items[2].quantity).to.equal(1);
+
+        console.log('Diff',JSON.stringify(jsonDiff.diff(res.body.orders,after), null, 4));
         return done(err);
       });
   });
